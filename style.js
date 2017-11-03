68d40b9a9372038a69769b744be52a4fa5c4da02
memetibryan <memetibryan@gmail.com>
2017-11-03 18:16:50 +0300

    changed index and style

--- style.js
+++ style.js
@@ -1,15 +1,3 @@
-$(".heading-compose").click(function() {
-      $(".side-two").css({
-        "left": "0"
-      });
-    });
-
-    $(".newMessage-back").click(function() {
-      $(".side-two").css({
-        "left": "-100%"
-      });
-    });
-    
-/*showing current time and date*/
+ /*showing current time and date*/
 var d = new Date()
 document.getElementById("time").innerHTML = d.toUTCString()
