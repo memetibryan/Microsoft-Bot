f876885d925b449a111134db90188f05688eb44d
memetibryan <memetibryan@gmail.com>
2017-11-11 04:06:53 +0300

    added the messgae to be transmitted

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
+//message to be sent
+var msg = `You ordered: ketchup for a total of 1000.`;
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
+    session.message.text;
+    session.send(msg);
 });
