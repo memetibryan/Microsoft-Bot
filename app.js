e816b03022fa38cc000bc80c60bdf4d3c7b250a5
memetibryan <memetibryan@gmail.com>
2017-11-16 09:26:25 +0300

    changed the passcode

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 8000, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
@@ -16,12 +16,7 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-//message to be sent
-var msg = `You ordered: ketchup for a total of 1000.`;
-
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
-    session.message.text;
-    session.send(msg);
 });
--- server.js
+++ server.js
@@ -4,6 +4,5 @@ connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
 });
 
-//bot passcode qorahtYBCB32%;}jVYW958]
-// MICROSOFT_APP_PASSWORD="ryeutTT3(reTGGEB0034)&\"
+//bot passcode tdnqA3@xmrJVXNJV7967(^_
 // MICROSOFT_APP_ID="5ed625c6-9062-4ed9-b616-b52c5df77fc2"
