b6d43c586fee678181fefe2722cb6ac2507bc94d
memetibryan <memetibryan@gmail.com>
2018-05-30 17:19:15 +0300

    started setup of firebase database

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 function sendText(){
       var message = document.getElementById("message").value;
       document.getElementById("messages").innerHTML = message;
+      console.log(message);
     };
